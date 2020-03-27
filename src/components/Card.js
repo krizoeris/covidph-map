04cb8a36a8a5ae6cef96c8449a0d39f784b4c0a8
krizoeris <krizoeris@yahoo.com.ph>
2020-03-28 00:52:20 +0400

    Fixed responsive design

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,41 @@ import React from 'react'
 
 const Card = ({cases, confirmed, recovered, death, handleCardClose}) => {
     return (
-        <div class="max-w-sm rounded-lg bg-blue-900 shadow-lg map-card p-5">
+        <div class="w-sm rounded-lg bg-blue-900 shadow-lg map-card p-3">
             <button className="close-card rounded-full bg-blue-800 hover:bg-blue-700 font-bold text-gray-300 shadow-large" onClick={handleCardClose}>
                 <i class="fas fa-times"></i>
             </button>
-            <div className="mb-4 mr-6 text-center grid grid-cols-3 gap-2">
-                <div class="p-2 sm:p-0 bg-orange-700 text-white rounded">
+            <div className="mr-6 text-center grid grid-cols-3 gap-2">
+                <div class="p-1 bg-orange-700 text-white rounded hidden lg:block md:block">
                     <p className="font-bold">Infected</p>
                     <p className="font-bold">{confirmed}</p>
                 </div>
-                <div class="p-2 sm:p-0 bg-red-700 text-white rounded">
+                <div class="p-1 bg-red-700 text-white rounded hidden lg:block md:block">
                     <p className="font-bold">Deaths</p>
                     <p className="font-bold">{death}</p>
                 </div>
-                <div class="p-2 sm:p-0 bg-green-700 text-white rounded">
+                <div class="p-1 bg-green-700 text-white rounded hidden lg:block md:block">
                     <p className="font-bold">Recovered</p>
                     <p className="font-bold">{recovered}</p>
                 </div>
             </div>
-            <div className="overflow-y-auto" style={{height: '85%'}}>
+
+            <div class="mb-3 mt-0 md:mt-3 lg:mt-3 mr-6 md:mr-0 lg:mr-0">
+                <button class="bg-white border-white border hover:bg-white text-blue-900 font-semibold rounded-l w-1/3">
+                    Cities
+                </button>
+                <button class="border-white border hover:bg-white hover:text-blue-900 text-white font-semibold w-1/3">
+                    Countries
+                </button>
+                <button class="border-white border hover:bg-white hover:text-blue-900 text-white font-semibold rounded-r w-1/3">
+                    Analitycs
+                </button>
+            </div>
+
+            <div className="mb-3">
+                <input class="bg-gray-600 rounded-full w-full py-2 px-4 text-white leading-tight focus:outline-none fontAwesome" type="text" placeholder="Search"></input>
+            </div>
+            <div className="overflow-y-auto map-card-content">
                 {cases.map(cases => (
                     <div class="p-3 mr-2 mt-0 mb-4 bg-blue-800 text-white rounded-lg">
                         <span className="">{cases.name}</span>
--- src/components/Credits.js
+++ src/components/Credits.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Credits = () => {
     return (
-        <div class="rounded-lg bg-blue-900 shadow-lg credits p-3 text-white font-semibold">
-            Made by <a href="https://github.com/krizoeris" target="_blank" className="text-blue-400">krizoeris</a> Data from <a className="text-blue-400" target="_blank" href="https://www.doh.gov.ph/2019-nCov">DOH</a>
+        <div class="rounded-lg bg-blue-900 shadow-lg credits p-1 text-white font-semibold">
+            <p>Made by <a href="https://github.com/krizoeris" target="_blank" className="text-blue-400">krizoeris</a> Data from <a className="text-blue-400" target="_blank" href="https://www.doh.gov.ph/2019-nCov">DOH</a></p>
         </div>
     )
 }
